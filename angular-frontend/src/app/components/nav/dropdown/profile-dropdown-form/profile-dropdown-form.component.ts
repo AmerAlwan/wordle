2ab132437fa15b4e9ff40651c6d377e56328047d
Amer Alwan <ameralwan@hotmail.com>
2022-03-16 16:56:49 -0400

    Added profile dropdown and dropdown menu animations as well as login/register modes

--- angular-frontend/src/app/components/nav/dropdown/profile-dropdown-form/profile-dropdown-form.component.ts
+++ angular-frontend/src/app/components/nav/dropdown/profile-dropdown-form/profile-dropdown-form.component.ts
@@ -1,21 +1,41 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-profile-dropdown-form',
   templateUrl: './profile-dropdown-form.component.html',
-  styleUrls: ['./profile-dropdown-form.component.css']
+  styleUrls: ['./profile-dropdown-form.component.css'],
+  animations: [
+    trigger('revealUsernameField', [
+      state('reveal', style({
+      })),
+      state('hide', style({
+        opacity: 0,
+        height: 0
+      })),
+      transition('hide => reveal', [
+        animate('0.2s 0.1s')
+      ]),
+      transition('reveal => hide', [
+        style({ opacity: 0 }),
+        animate('0.3s 0.1s')
+      ])
+    ])
+  ]
 })
 export class ProfileDropdownFormComponent implements OnInit {
-  @Input() showRegister: boolean = false;
-  @Input() showLogin: boolean = false;
+  @Input() isDropdownOpen: boolean = false;
+  isRegisterMode: boolean = false;
 
-  constructor() { }
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
-  registerToggle() {
-    this.showRegister = false ? true : false;
+  toggleRegisterMode() {
+    this.isRegisterMode = !this.isRegisterMode;
   }
 
 }
--- angular-frontend/src/app/components/nav/dropdown/profile-dropdown/profile-dropdown.component.ts
+++ angular-frontend/src/app/components/nav/dropdown/profile-dropdown/profile-dropdown.component.ts
@@ -1,5 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, Input, OnInit } from '@angular/core';
 import { trigger, state, style, transition, animate } from '@angular/animations';
+import { ViewChild } from '@angular/core';
+import { Renderer2 } from '@angular/core';
+import { AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-profile-dropdown',
@@ -10,22 +13,43 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
       state('extend', style({
         width: '300px'
       })),
-      transition('retreat <=> extend', [
+      transition('retreat => extend', [
         animate('0.2s')
+      ]),
+      transition('extend => retreat', [
+        animate('0.2s 0.3s')
       ])
     ]),
+    trigger('revealMenu', [
+      state('extend', style({
+        opacity: 1,
+      })),
+      state('retreat', style({
+        opacity: 0,
+        height: 0
+      })),
+      transition('retreat <=> extend', [
+        animate('0.3s 0.3s ease-in-out')
+      ])
+    ])
   ]
 })
 
 
-export class ProfileDropdownComponent implements OnInit {
+export class ProfileDropdownComponent implements OnInit, AfterViewInit {
   username: string = "Anonymous";
   isDropdownOpen: boolean = false;
 
-  constructor() { }
+  constructor(private renderer: Renderer2) {
+    
+  }
 
   ngOnInit(): void {
   }
+
+  ngAfterViewInit() {
+  
+}
 
   setIsDropdownOpen(open: boolean) {
     this.isDropdownOpen = open;
