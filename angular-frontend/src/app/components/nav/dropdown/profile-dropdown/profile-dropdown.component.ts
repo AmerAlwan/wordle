a568cab598ee84b6a83bf6816ffdd170d4829cc3
Yahia-Sultan <sul,yahia@gmail.com>
2022-03-15 23:09:19 -0400

    Dropdown form update

--- angular-frontend/src/app/components/nav/dropdown/profile-dropdown/profile-dropdown.component.ts
+++ angular-frontend/src/app/components/nav/dropdown/profile-dropdown/profile-dropdown.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-profile-dropdown',
   templateUrl: './profile-dropdown.component.html',
   styleUrls: ['./profile-dropdown.component.css']
 })
+
+
 export class ProfileDropdownComponent implements OnInit {
+  @Input() showRegister: boolean = false;
+  @Input() showLogin: boolean = false;
 
   constructor() { }
+
+  registerToggle() {
+    this.showRegister = false ? true : false;
+  }
 
   ngOnInit(): void {
   }
 
 }
+
