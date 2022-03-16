2cdc24201d5abd96eca90c0cd8c01de6a27e518a
Amer Alwan <ameralwan@hotmail.com>
2022-03-16 02:05:28 -0400

    Navbar revamped

--- angular-frontend/src/app/components/nav/dropdown/profile-dropdown/profile-dropdown.component.ts
+++ angular-frontend/src/app/components/nav/dropdown/profile-dropdown/profile-dropdown.component.ts
@@ -1,23 +1,34 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-profile-dropdown',
   templateUrl: './profile-dropdown.component.html',
-  styleUrls: ['./profile-dropdown.component.css']
+  styleUrls: ['./profile-dropdown.component.css'],
+  animations: [
+    trigger('extendButton', [
+      state('extend', style({
+        width: '300px'
+      })),
+      transition('retreat <=> extend', [
+        animate('0.2s')
+      ])
+    ]),
+  ]
 })
 
 
 export class ProfileDropdownComponent implements OnInit {
-  @Input() showRegister: boolean = false;
-  @Input() showLogin: boolean = false;
+  username: string = "Anonymous";
+  isDropdownOpen: boolean = false;
 
   constructor() { }
 
-  registerToggle() {
-    this.showRegister = false ? true : false;
+  ngOnInit(): void {
   }
 
-  ngOnInit(): void {
+  setIsDropdownOpen(open: boolean) {
+    this.isDropdownOpen = open;
   }
 
 }
